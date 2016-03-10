313fed9edc1a839cc095fca9250b5922192b1f91
skellyb <skellyb@gmail.com>
2016-03-09 18:04:46 -0600

    Instead of subclassing React.Component use Higher-Ordered Component

--- src/index.js
+++ src/index.js
@@ -1,55 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
 import d3 from 'd3'
-import assign from 'lodash.assign'
 
-export default class ReactD3 extends React.Component {
-
-  initialize (svg, data, options) {}
-
-  update (svg, data, options) {}
-
-  destroy () {}
-
-  componentDidMount () {
-    this.initialize(React.findDOMNode(this), this.props.data, this._getOptions(this.props.options))
-    this.update(React.findDOMNode(this), this.props.data, this._getOptions(this.props.options))
+export default function d3Wrap (lifecycle) {
+  const d3Lifecycle = {
+    initialize (svg, data, options) {},
+    update (svg, data, options) {},
+    destroy () {},
+    ...lifecycle
   }
 
-  componentDidUpdate () {
-    this.update(React.findDOMNode(this), this.props.data, this._getOptions(this.props.options))
-  }
-
-  componentWillUnmount () {
-    this.destroy()
-  }
-
-  render () {
-    var classes = 'd3-wrap'
-    if (this.props.className) {
-      classes = classes.concat(' ', this.props.className)
+  class D3Wrap extends Component {
+    componentDidMount () {
+      d3Lifecycle.initialize.call(this, findDOMNode(this), this.props.data, this._getOptions(this.props.options))
+      d3Lifecycle.update.call(this, findDOMNode(this), this.props.data, this._getOptions(this.props.options))
     }
 
-    return <svg className={ classes } width={ this.props.width } height={ this.props.height } />
+    componentDidUpdate () {
+      d3Lifecycle.update.call(this, findDOMNode(this), this.props.data, this._getOptions(this.props.options))
+    }
+
+    componentWillUnmount () {
+      d3Lifecycle.destroy.call(this, )
+    }
+
+    _getOptions (propOps) {
+      const options = (typeof propOps === 'undefined') ? {} : propOps
+      return {
+        margin: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+        },
+        xaxis: { orientation: 'bottom' },
+        yaxis: { orientation: 'left' },
+        ...options
+      }
+    }
+
+    render () {
+      const { className, width, height } = this.props
+      let classes = 'd3-wrap'
+      if (className) classes = classes.concat(' ', this.props.className)
+
+      return <svg className={ classes } width={ width } height={ height } />
+    }
   }
 
-  _getOptions (propOps) {
-    const options = (typeof propOps === 'undefined') ? {} : propOps
-    return assign({
-      margin: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-      },
-      xaxis: { orientation: 'bottom' },
-      yaxis: { orientation: 'left' }
-    }, options)
+  D3Wrap.propTypes = {
+    data: React.PropTypes.array.isRequired,
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    options: React.PropTypes.object
   }
-}
 
-ReactD3.propTypes = {
-  data: React.PropTypes.array.isRequired,
-  width: React.PropTypes.number.isRequired,
-  height: React.PropTypes.number.isRequired,
-  options: React.PropTypes.object
+  return D3Wrap
 }
